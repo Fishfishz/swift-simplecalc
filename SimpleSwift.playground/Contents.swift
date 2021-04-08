bbb597e1609dc1c096d2734ccbf075084b5336f7
Fishzz <fishzz@Fishzzs-MacBook-Pro.local>
2021-04-08 12:00:21 -0700

    basic complete

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,56 @@
+import Foundation
+
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    switch args.last {
+    case "avg":
+        if args.count == 1 {
+            return 0
+        }
+        var sum = 0;
+        for i in 0...(args.count - 1) {
+            sum += Int(args[i]) ?? 0
+        }
+        return sum / (args.count - 1)
+    case "fact":
+        if args.count == 1 {
+            return 0
+        }
+        let num = Int(args[0]) ?? 0
+        var index = 1
+        var total = 1
+        while index <= num {
+            total = total * index
+            index += 1
+        }
+        return total
+    case "count" :
+        return args.count - 1
+    default:
+        let num1 = Int(args[0]) ?? 0
+        let num2 = Int(args[2]) ?? 0
+        switch args[1] {
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case "%":
+            return num1 % num2
+        default:
+            return 0;
+            
+        }
+    }
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    let args = arg.split(separator: " ").map({(subsequnce) in return String(subsequnce)})
+    return calculate(args)
 }
 
 // -------------------------------------------
@@ -24,13 +69,13 @@ calculate(["1", "2", "3", "4", "5", "count"]) == 5
 calculate(["count"]) == 0
 
 calculate(["1", "2", "3", "4", "5", "avg"]) == 3
-    // 15 / 5 = 3
+// 15 / 5 = 3
 calculate(["2", "2", "4", "4", "avg"]) == 3
-    // 12 / 4 = 3
+// 12 / 4 = 3
 calculate(["2", "avg"]) == 2
-    // 2 / 1 = 2
+// 2 / 1 = 2
 calculate(["avg"]) == 0
-    // 0 / 0 = 0 (not really, but it's an edge case
+// 0 / 0 = 0 (not really, but it's an edge case
 
 calculate(["0", "fact"]) == 1
 calculate(["1", "fact"]) == 1
